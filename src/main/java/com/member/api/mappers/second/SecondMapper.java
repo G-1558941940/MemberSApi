e0a49d7b55efa36c95b99632c2cea9eb7cb2a089
chensiqi <1450983391@qq.com>
2020-06-30 15:52:28 +0800

    会员信息查询 --chensiqi

--- src/main/java/com/member/api/mappers/second/SecondMapper.java
+++ src/main/java/com/member/api/mappers/second/SecondMapper.java
@@ -1,7 +1,6 @@
 package com.member.api.mappers.second;
 
 import com.member.api.entity.Car_owner;
-import com.member.api.entity.OutParking;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -9,11 +8,13 @@ import java.util.List;
 @Component
 public interface SecondMapper {
 
-    /**查询会员信息
+    /**
+     * 查询会员信息
+     *
      * @param pageIndex 页码
      * @param pageSize  每页长度
      * @return Car_owner 集合
      */
-public List<Car_owner> Car_owner(Integer pageIndex,Integer pageSize);
-//public List<> Outstanding_order();
+    List<Car_owner> Car_owner(Integer pageIndex, Integer pageSize);
+
 }
